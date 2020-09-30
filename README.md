51c7743ff28d0a6120ea2c61e64bde0145e25f4a
mechlib <72147558+mechlib@users.noreply.github.com>
2020-09-30 20:19:13 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+"# mechlib.github.io" 
---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    // Setup - add a text input to each footer cell
+    $('#example tfoot th').each( function () {
+        var title = $(this).text();
+        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+    } );
+ 
+    // DataTable
+    var table = $('#example').DataTable({
+        initComplete: function () {
+            // Apply the search
+            this.api().columns().every( function () {
+                var that = this;
+ 
+                $( 'input', this.footer() ).on( 'keyup change clear', function () {
+                    if ( that.search() !== this.value ) {
+                        that
+                            .search( this.value )
+                            .draw();
+                    }
+                } );
+            } );
+        }
+    });
+ 
+} );
